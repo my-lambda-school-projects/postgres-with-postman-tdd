2f86cd37f0bf33c0dd018fb9de3b5aadde6bbc4a
jamespagedev <jamespagedev@outlook.com>
2019-02-10 23:45:20 -0700

    testing with heroku v4

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const dbEngine = process.env.DATABASE_URL || 'development';
+const dbEngine = process.env.NODE_ENV || 'development';
 
 module.exports = knex(config[dbEngine]);
