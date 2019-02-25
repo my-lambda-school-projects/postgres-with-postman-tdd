da911a7e4586bd647c9d071d5d524acc424cf91c
jamespagedev <jamespagedev@outlook.com>
2019-02-24 22:00:14 -0700

    use localPG

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const dbEngine = process.env.DATABASE || 'development';
+const dbEngine = process.env.DB || 'development';
 
 module.exports = knex(config[dbEngine]);
